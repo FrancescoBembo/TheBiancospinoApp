addb8f5954f1c8a97792be328a950417e2a45b25
FrancescoBembo <bembofrancesco@icloud.com>
2020-05-20 15:22:38 +1000

    - working skip btn in the onboarding screens

--- app/src/main/java/com/example/thebiancospinoapp/IntroActivity.java
+++ app/src/main/java/com/example/thebiancospinoapp/IntroActivity.java
@@ -3,12 +3,17 @@ package com.example.thebiancospinoapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class IntroActivity extends AppCompatActivity {
+
+    private Button BtnSkip;
 
     private ViewPager screenPager;
     IntroviewPagerAdapter introviewPagerAdapter;
@@ -20,14 +25,35 @@ public class IntroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_intro);
 
         List<ScreenItem> mList = new ArrayList<>();
-        mList.add(new ScreenItem("Shop Anywhere", "dadfs adfadf fd,\nad fa dfdad fdfsdfadf", R.drawable.onb1));
-        mList.add(new ScreenItem("New recipes", "dadfs ad fhh hhadf fd,\nad fa dfdj ad fdfsdfadf", R.drawable.onb2));
-        mList.add(new ScreenItem("Get points", "dadfs  fd,\nad fa dfdad g fdfsdfadf", R.drawable.onb3));
+        mList.add(new ScreenItem("Shop anytime, anywhere!", "Shop at any time and decide whether to pick up your\norder or get it delivered at your doorstep.", R.drawable.onb1));
+        mList.add(new ScreenItem("Discover new recipes!", "Discover new weekly recipes, and buy \nall the ingredients with one tap.", R.drawable.onb2));
+        mList.add(new ScreenItem("Earn points and rewards!", "Earn points with every purchase on the app on in\nstore and unlock rewards and discounts.", R.drawable.onb3));
 
         screenPager =findViewById(R.id.screen_viewpager);
         introviewPagerAdapter = new IntroviewPagerAdapter(this,mList);
 
         screenPager.setAdapter(introviewPagerAdapter);
+
+
+
+        BtnSkip = findViewById(R.id.btn_skip);
+        BtnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                moveToMainActivity();
+
+            }
+        });
+
+
+
+    }
+
+    private void moveToMainActivity(){
+
+        Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+        startActivity(intent);
 
     }
 }
