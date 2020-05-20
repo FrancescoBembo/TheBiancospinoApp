d57156896ea4d3f8377ae681402410e23beec68c
FrancescoBembo <bembofrancesco@icloud.com>
2020-05-20 11:51:45 +1000

    Initial commit - bottom navigation bar - fragment implementation - home image btns - Account page

---
+++ app/src/main/java/com/example/thebiancospinoapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.thebiancospinoapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new HomeFragment()).commit();
+
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new
+            BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                    Fragment fragment = null;
+
+                    switch (menuItem.getItemId())
+                    {
+                        case R.id.home:
+                            fragment = new HomeFragment();
+                            break;
+
+                        case R.id.shop:
+                            fragment = new ShopFragment();
+                            break;
+
+                        case R.id.recipes:
+                            fragment = new RecipesFragment();
+                            break;
+
+                        case R.id.account:
+                            fragment = new AccountFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+
+
+                    return true;
+                }
+            };
+
+}
