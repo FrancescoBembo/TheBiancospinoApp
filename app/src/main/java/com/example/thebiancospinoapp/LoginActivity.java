bbd8f13e1136c5e04f433bdbdfa39f9354269923
FrancescoBembo <bembofrancesco@icloud.com>
2020-05-23 11:12:12 +1000

    - LoginBtn linked to Intro Activity

--- app/src/main/java/com/example/thebiancospinoapp/LoginActivity.java
+++ app/src/main/java/com/example/thebiancospinoapp/LoginActivity.java
@@ -2,13 +2,50 @@ package com.example.thebiancospinoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginButton;
+    Button goRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.loginBtn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent goToUserPageIntent = new Intent(LoginActivity.this, IntroActivity.class);
+
+                startActivity(goToUserPageIntent);
+
+            }
+        });
+
+
+        //GO TO REGISTER PAGE
+
+        /*goRegister = findViewById(R.id.goToRegistrationBtn);
+
+        goRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent goToRegistrationIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+
+                startActivity(goToRegistrationIntent);
+
+            }
+        });*/
+
+
     }
 }
