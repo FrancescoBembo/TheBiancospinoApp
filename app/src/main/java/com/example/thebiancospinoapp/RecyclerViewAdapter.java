fb7d773c346257e7ed18ac92bde4b91d207969f2
FrancescoBembo <bembofrancesco@icloud.com>
2020-05-28 10:18:12 +1000

    - login btn txt size fix - landscape adaptation register page

---
+++ app/src/main/java/com/example/thebiancospinoapp/RecyclerViewAdapter.java
@@ -0,0 +1,94 @@
+package com.example.thebiancospinoapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private ArrayList<String> mTitles = new ArrayList<>();
+    private ArrayList<String> mProd_prices = new ArrayList<>();
+    private ArrayList<String> mThumbnails = new ArrayList<>();
+    private com.example.thebiancospinoapp.ShopFragment ShopFragment;
+
+//    public RecyclerViewAdapter(ArrayList<String> mTitles, ArrayList<String> mProd_prices, ArrayList<String> mThumbnails, Context mContext) {
+//        this.mTitles = mTitles;
+//        this.mProd_prices = mProd_prices;
+//        this.mThumbnails = mThumbnails;
+//        this.mContext = mContext;
+//    }
+
+    public RecyclerViewAdapter(ShopFragment shopFragment, ArrayList<String> mTitles, ArrayList<String> mProd_prices, ArrayList<String> mThumbnails) {
+        this.mTitles = mTitles;
+        this.mProd_prices = mProd_prices;
+        this.mThumbnails = mThumbnails;
+        this.ShopFragment = ShopFragment;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+
+        Glide.with(ShopFragment)
+                .asBitmap()
+                .load(mThumbnails.get(position))
+                .into(holder.thumbnail);
+
+        holder.title.setText(mTitles.get(position));
+
+        holder.prod_price.setText(mProd_prices.get(position));
+
+        holder.parent_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Toast.makeText(ShopFragment, mTitles.get(position), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTitles.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView thumbnail;
+        TextView title;
+        TextView prod_price;
+        LinearLayout parent_layout;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            thumbnail = itemView.findViewById(R.id.thumbnail);
+            title = itemView.findViewById(R.id.title);
+            prod_price = itemView.findViewById(R.id.prod_price);
+            parent_layout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
--- app/src/main/java/com/example/thebiancospinoapp/ShopFragment.java
+++ app/src/main/java/com/example/thebiancospinoapp/ShopFragment.java
@@ -1,28 +1,106 @@
 package com.example.thebiancospinoapp;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentController;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.dynamic.FragmentWrapper;
+
+import java.util.ArrayList;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ShopFragment extends Fragment {
+public class ShopFragment extends Fragment{
 
     public ShopFragment() {
         // Required empty public constructor
     }
 
 
+    private ArrayList<String> mTitles = new ArrayList<>();
+    private ArrayList<String> mProd_prices = new ArrayList<>();
+    private ArrayList<String> mThumbnailsUrl = new ArrayList<>();
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+        initImageBitmaps();
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_shop, container, false);
+
+
+
     }
+
+    private void initImageBitmaps(){
+
+        mThumbnailsUrl.add("");
+        mTitles.add("Product 1");
+        mProd_prices.add("$5.99");
+
+        mThumbnailsUrl.add("");
+        mTitles.add("Product 1");
+        mProd_prices.add("$5.99");
+
+        mThumbnailsUrl.add("");
+        mTitles.add("Product 1");
+        mProd_prices.add("$5.99");
+
+        mThumbnailsUrl.add("");
+        mTitles.add("Product 1");
+        mProd_prices.add("$5.99");
+
+        mThumbnailsUrl.add("");
+        mTitles.add("Product 1");
+        mProd_prices.add("$5.99");
+
+        mThumbnailsUrl.add("");
+        mTitles.add("Product 1");
+        mProd_prices.add("$5.99");
+
+        mThumbnailsUrl.add("");
+        mTitles.add("Product 1");
+        mProd_prices.add("$5.99");
+
+        mThumbnailsUrl.add("");
+        mTitles.add("Product 1");
+        mProd_prices.add("$5.99");
+
+        mThumbnailsUrl.add("");
+        mTitles.add("Product 1");
+        mProd_prices.add("$5.99");
+
+        mThumbnailsUrl.add("");
+        mTitles.add("Product 1");
+        mProd_prices.add("$5.99");
+
+        initRecyclerView();
+
+    }
+
+
+    private void initRecyclerView(){
+
+        RecyclerView recyclerView = getView().findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mTitles, mProd_prices, mThumbnailsUrl );
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+
+
+    }
+
 }
