5fa8fbf04a41b2325e29620264980b1d564818f8
FrancescoBembo <bembofrancesco@icloud.com>
2020-05-29 07:02:24 +1000

    - shop fragment UI fixes

--- app/src/main/java/com/example/thebiancospinoapp/ShopFragment.java
+++ app/src/main/java/com/example/thebiancospinoapp/ShopFragment.java
@@ -53,45 +53,45 @@ public class ShopFragment extends Fragment{
 
     private void initImageBitmaps(){
 
-        mThumbnailsUrl.add("https://firebasestorage.googleapis.com/v0/b/thebiancospinoapp.appspot.com/o/Product1.png?alt=media&token=dbd69ab0-5daa-4dc8-8e1d-1e98ee1a960f");
+        mThumbnailsUrl.add("https://firebasestorage.googleapis.com/v0/b/thebiancospinoapp.appspot.com/o/P1.png?alt=media&token=6e70d36f-2e06-4ffc-93ee-4a3fdfb39d3f");
         mTitles.add("Pasta kamut");
         mProd_prices.add("$3.99");
 
-        mThumbnailsUrl.add("");
-        mTitles.add("Product 2");
-        mProd_prices.add("$5.99");
+        mThumbnailsUrl.add("https://firebasestorage.googleapis.com/v0/b/thebiancospinoapp.appspot.com/o/P2.png?alt=media&token=86745114-c445-4cdd-a980-df320302184e");
+        mTitles.add("Tagliatelle farro");
+        mProd_prices.add("$2.99");
 
-        mThumbnailsUrl.add("");
-        mTitles.add("Product 3");
-        mProd_prices.add("$5.99");
+        mThumbnailsUrl.add("https://firebasestorage.googleapis.com/v0/b/thebiancospinoapp.appspot.com/o/P3.png?alt=media&token=962a1cc2-86df-410e-bcfe-c78a23aaed50");
+        mTitles.add("Cracker farro");
+        mProd_prices.add("$3.50");
 
-        mThumbnailsUrl.add("");
-        mTitles.add("Product 4");
-        mProd_prices.add("$5.99");
+        mThumbnailsUrl.add("https://firebasestorage.googleapis.com/v0/b/thebiancospinoapp.appspot.com/o/P4.png?alt=media&token=a9f4caee-770b-4832-80de-1fd60d5c6c76");
+        mTitles.add("SoftCrock rosemary");
+        mProd_prices.add("$1.99");
 
-        mThumbnailsUrl.add("");
-        mTitles.add("Product 5");
-        mProd_prices.add("$5.99");
+        mThumbnailsUrl.add("https://firebasestorage.googleapis.com/v0/b/thebiancospinoapp.appspot.com/o/P5.png?alt=media&token=77f28bb3-3184-4209-b3b3-16c5182fd7cb");
+        mTitles.add("Mais balls");
+        mProd_prices.add("$1.99");
 
-        mThumbnailsUrl.add("");
-        mTitles.add("Product 6");
-        mProd_prices.add("$5.99");
+        mThumbnailsUrl.add("https://firebasestorage.googleapis.com/v0/b/thebiancospinoapp.appspot.com/o/P6.png?alt=media&token=29d20ba1-8d68-43eb-8350-2104111cb03a");
+        mTitles.add("Cooking salt");
+        mProd_prices.add("$2.99");
 
-        mThumbnailsUrl.add("");
-        mTitles.add("Product 7");
-        mProd_prices.add("$5.99");
+        mThumbnailsUrl.add("https://firebasestorage.googleapis.com/v0/b/thebiancospinoapp.appspot.com/o/P7.png?alt=media&token=1cfee4ac-7bf3-429c-b639-cc10c1c52fab");
+        mTitles.add("Goji berries");
+        mProd_prices.add("$5.50");
 
-        mThumbnailsUrl.add("");
-        mTitles.add("Product 8");
-        mProd_prices.add("$5.99");
+        mThumbnailsUrl.add("https://firebasestorage.googleapis.com/v0/b/thebiancospinoapp.appspot.com/o/P8.png?alt=media&token=a44f73f0-316a-46e1-a0e6-b24321c56189");
+        mTitles.add("Choco spread");
+        mProd_prices.add("$4.99");
 
-        mThumbnailsUrl.add("");
-        mTitles.add("Product 9");
-        mProd_prices.add("$5.99");
+        mThumbnailsUrl.add("https://firebasestorage.googleapis.com/v0/b/thebiancospinoapp.appspot.com/o/P9.png?alt=media&token=db203ca4-15e7-44d7-8b25-7b03be0b04e0");
+        mTitles.add("Soya choc dessert");
+        mProd_prices.add("$3.99");
 
-        mThumbnailsUrl.add("");
-        mTitles.add("Product 10");
-        mProd_prices.add("$5.99");
+        mThumbnailsUrl.add("https://firebasestorage.googleapis.com/v0/b/thebiancospinoapp.appspot.com/o/P10.png?alt=media&token=f7083f1c-c7c6-4fe9-8c91-86236ba6a6ee");
+        mTitles.add("Soya van dessert");
+        mProd_prices.add("$3.99");
 
         // initRecyclerView();
 
